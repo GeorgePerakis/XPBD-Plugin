82f60b8892e33c2e3c0e3cba1a1b7f193df6153d
Giorgos Perakis <perakis@exeo.gr>
2026-04-01 15:40:23 +0300

    Updated pseudo solver, implemented constructors

--- src/structs/Distance_Constraint.hpp
+++ src/structs/Distance_Constraint.hpp
@@ -5,4 +5,6 @@ struct Distance_Constraint {
     int particle_b;
     double rest_length;
     double stiffness;
+
+    Distance_Constraint(int a, int b, double rest_len, double stiff) : particle_a(a), particle_b(b), rest_length(rest_len), stiffness(stiff) {}
 };
--- src/structs/Particle.hpp
+++ src/structs/Particle.hpp
@@ -6,4 +6,6 @@ struct Particle {
     Vec2 position;
     Vec2 old_position;
     float inverse_mass;
+
+    Particle(Vec2 pos, float inv_mass) : position(pos), old_position(pos), inverse_mass(inv_mass) {}
 };
